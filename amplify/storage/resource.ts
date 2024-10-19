eae48c60bf26d5977f41fcbf9430ce06f70b39fe
unknown <fahmiemike@gmail.com>
2024-10-19 18:08:02 -0400

    Fix storage access path configuration

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'spookystockbucket',
   access: (allow) => ({
-    '/*': [ // This allows access to all paths in the bucket
+    '*': [ // This allows access to all paths in the bucket
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
   })
